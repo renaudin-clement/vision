7a6a400b54169cf526ff00297aacec36081f88e2
renaudin clement <pro.renaudin.clement@gmail.com>
2025-05-22 11:43:40 +0200

    premiere animation css

--- cursors.js
+++ cursors.js
@@ -3,3 +3,12 @@ const cursor = document.querySelector('.cursors');
 document.addEventListener('mousemove', e =>{
     cursor.setAttribute('style','top:'+(e.pageY - 20)+'px; left:'+(e.pageX - 20)+'px')
 });
+
+document.addEventListener('click',e =>{
+cursor.classList.add('expand');
+
+setTimeout(() => {
+    cursor.classList.remove('expand');
+}, 500);
+
+});
